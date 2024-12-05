35804d18362af442e2ffad410ebbffc50c088ca4
Toni Kemppainen <tonikem@gmail.com>
2024-12-05 17:39:43 +0200

    Cropped main character

--- game.py
+++ game.py
@@ -1,21 +1,31 @@
 import sys
 import pygame
 
-pygame.init()
 
-pygame.display.set_caption("Platform Game")
-screen = pygame.display.set_mode((640, 480))
-clock = pygame.time.Clock()
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption("Platform Game")
+        self.screen = pygame.display.set_mode((640, 480))
+        self.clock = pygame.time.Clock()
+
+        self.hero_img = pygame.image.load("assets/sprites/Hero.png").convert()
+
+    def run(self):
+        while True:
+            cropped_region = (0, 20, 14, 28)
+            self.screen.blit(self.hero_img, (0, 0), cropped_region)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.update()
+            self.clock.tick(60)
 
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
 
-    pygame.display.update()
-    clock.tick(60)
-
+Game().run()
 
 
