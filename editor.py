80a3b9e70bc645966e9dd9c044ca54165a5eec9a
Toni Kemppainen <tonikem@gmail.com>
2024-12-09 09:22:54 +0200

    Kameran liikuttelu

--- editor.py
+++ editor.py
@@ -34,6 +34,9 @@ class Editor:
         while True:
             self.display.fill((0, 0, 0))
 
+            self.scroll[0] += (self.movement[1] - self.movement[0]) * 2
+            self.scroll[1] += (self.movement[3] - self.movement[2]) * 2
+
             render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
             self.tilemap.render(self.display, offset=render_scroll)
 
